3de96260a16781b6e83bf35754c9ce3c3e97f071
Codeyg12 <codey.gallup@gmail.com>
2022-08-25 19:28:50 -0600

    fixed previous buttons issue:

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,30 @@
 let newRowEl = $("<tr>");
-let currentWeatherData = $("#currentConditions");
-let recentSearches = $("#recentSearches");
+let currentWeatherData = $("#currentCondition");
 let previousCities = [];
-
-
 
 function searchHistory() {
   previousCities = JSON.parse(localStorage.getItem("search"));
 
+  $(".recentSearches").empty()
   $(previousCities).each(function (i) {
     let locationBtn = $("<button>")
       .text(previousCities[i])
       .addClass("localBtn button is-dark ml-1 has-text-centered mt-3");
     $(".recentSearches").append(locationBtn);
-    $(locationBtn).click(function (e) {
-      currentConditions(e.target.dataset.city)
-    })
+  });
+  $(".localBtn").click(function (e) {
+    currentConditions(e.target.innerHTML);
   });
 }
 
+
 function updateStorage(city) {
-  if (previousCities.includes(city)) {
-    return;
-  } else {
+  if (!previousCities.includes(city)) {
     previousCities.push(city);
-    
+
     localStorage.setItem("search", JSON.stringify(previousCities));
 
-    searchHistory()
+    searchHistory();
   }
 }
 
@@ -43,6 +40,7 @@ function currentConditions(city) {
     method: "GET",
   }).then(function (data) {
     console.log(data);
+    $("#currentCondition").empty()
     //Creating new elements to place into table
     let cityName = data.name;
     let nameData = $("<td>").append(city).addClass("is-size-3");
@@ -62,28 +60,26 @@ function currentConditions(city) {
       .addClass("is-size-3");
 
     //Appends the new elements onto the page
-    newRowEl.append(nameData, tempData, weatherIconEl, humidData, windData);
-    currentWeatherData.append(newRowEl);
 
     //Grabs lat and lon for new fetch
     let lat = data.coord.lat;
     let long = data.coord.lon;
 
     //Uses new lat and lon for more specific answers
-    fiveDayForecast(lat, long);
+    fiveDayForecast(lat, long, nameData, tempData, weatherIconEl, humidData, windData);
     updateStorage(cityName);
   });
 }
 
 //Five day forecast
-function fiveDayForecast(lat, long) {
+function fiveDayForecast(lat, long, name, temp, weather, humid, wind) {
   let apiCall =
-  "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-  lat +
-  "&lon=" +
-  long +
-  "&exclude=alerts,hourly,minutely&appid=0830fec5fefb765b207129fdb7fcdf86&units=imperial";
-  
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    long +
+    "&exclude=alerts,hourly,minutely&appid=0830fec5fefb765b207129fdb7fcdf86&units=imperial";
+
   $.ajax({
     url: apiCall,
     method: "GET",
@@ -91,7 +87,7 @@ function fiveDayForecast(lat, long) {
     //New element for UVI
     let uviLog = data.current.uvi;
     let uviData = $("<td>").text(uviLog);
-    
+
     //Color coding the block for UVI based on results
     if (uviLog < 3) {
       uviData.addClass("is-size-3 has-background-success");
@@ -104,15 +100,14 @@ function fiveDayForecast(lat, long) {
     } else {
       uviData.addClass("is-size-3 has-text-white tempSwelter");
     }
-    newRowEl.append(uviData);
-    currentWeatherData.append(newRowEl);
-    
-   
+    newRowEl.append(name, temp, weather, humid, wind, uviData);
+     currentWeatherData.append(newRowEl);
+
     $("#fiveDayTable").empty();
     postForecast(data);
-    
   });
 }
+
 
 //Populates the results from 5 day
 function postForecast(data) {
@@ -129,25 +124,24 @@ function postForecast(data) {
         "http://openweathermap.org/img/wn/" + forecastWeatherIcon + "@2x.png"
       );
       let forecastTemp = $("<td>")
-      .text(data.daily[i].temp.day + "℉")
+        .text(data.daily[i].temp.day + "℉")
         .addClass("is-size-3");
-        let forecastHumidity = $("<td>")
+      let forecastHumidity = $("<td>")
         .text(data.daily[i].humidity + "%")
         .addClass("is-size-3");
-        let forecastWinds = $("<td>")
+      let forecastWinds = $("<td>")
         .text(data.daily[i].wind_speed + "MPH")
         .addClass("is-size-3");
-        
-        //appends to table
-        otherRow.append(
-          daysDate,
-          forecastTemp,
-          forecastIcon,
-          forecastHumidity,
-          forecastWinds
-          );
+
+      //appends to table
+      otherRow.append(
+        daysDate,
+        forecastTemp,
+        forecastIcon,
+        forecastHumidity,
+        forecastWinds
+      );
       $("#fiveDayTable").append(otherRow);
-      
     }
   });
 }
@@ -159,7 +153,6 @@ $("#citySelect").click(function (e) {
   $("#city").val("");
   currentConditions(selectedCity);
 });
-
 
 $("#clear").click(function () {
   localStorage.clear();
