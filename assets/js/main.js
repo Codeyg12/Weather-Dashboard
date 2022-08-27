763dc9d7a60acabd0cf5a269d52f0903fc716a49
Codeyg12 <codey.gallup@gmail.com>
2022-08-27 10:20:11 -0600

    fixed current conditions and uv

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 let newRowEl = $("<tr>");
-let currentWeatherData = $("#currentCondition");
+let currentWeatherData = $(".currentWeather");
 let previousCities = [];
 
 function searchHistory() {
@@ -43,30 +43,21 @@ function currentConditions(city) {
     $("#currentCondition").empty()
     //Creating new elements to place into table
     let cityName = data.name;
-    let nameData = $("<td>").append(city).addClass("is-size-3");
+
     let weatherIcon = data.weather[0].icon;
-    let weatherIconEl = $("<img>").attr(
-      "src",
-      "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
-    );
-    let tempData = $("<td>")
-      .text(data.main.temp + "℉")
-      .addClass("is-size-3");
-    let humidData = $("<td>")
-      .text(data.main.humidity + "%")
-      .addClass("is-size-3");
-    let windData = $("<td>")
-      .text(data.wind.speed + "MPH")
-      .addClass("is-size-3");
+    let weatherIconEl = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+    let tempData = data.main.temp + "℉"
+    let humidData = data.main.humidity + "%"
+    let windData = data.wind.speed + "MPH"
 
     //Appends the new elements onto the page
-
+    
     //Grabs lat and lon for new fetch
     let lat = data.coord.lat;
     let long = data.coord.lon;
 
     //Uses new lat and lon for more specific answers
-    fiveDayForecast(lat, long, nameData, tempData, weatherIconEl, humidData, windData);
+    fiveDayForecast(lat, long, cityName, tempData, weatherIconEl, humidData, windData);
     updateStorage(cityName);
   });
 }
@@ -74,34 +65,48 @@ function currentConditions(city) {
 //Five day forecast
 function fiveDayForecast(lat, long, name, temp, weather, humid, wind) {
   let apiCall =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-    lat +
+  "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+  lat +
     "&lon=" +
     long +
     "&exclude=alerts,hourly,minutely&appid=0830fec5fefb765b207129fdb7fcdf86&units=imperial";
-
-  $.ajax({
+    $.ajax({
     url: apiCall,
     method: "GET",
   }).then(function (data) {
+    let date = dayjs().format("MM/DD/YYYY")
     //New element for UVI
-    let uviLog = data.current.uvi;
-    let uviData = $("<td>").text(uviLog);
-
+    let uviLog= data.current.uvi;
+  
+    let currentWeatherInfo = `
+    <h2 class="subtitle has-text-centered is-size-3 has-text-black"> 
+    Current Weather Conditions <img src="${weather}">
+  </h2>
+    <h3 class="is-size-3 has-text-centered mb-2">${name} ${date}</h3>
+    <ul id="currentList">
+      <li class="is-size-4 has-text-centered">Temperature: ${temp}</li>
+      <li class="is-size-4 has-text-centered">Wind Speeds: ${wind}</li>
+      <li class="is-size-4 has-text-centered">Humidity: ${humid}</li>
+      <li class="is-size-4 has-text-centered" id="uvi">UV Index:</li>
+      </ul>`
+      
+      currentWeatherData.html(currentWeatherInfo);
+      $("#uvi").html(`UV Index: <span class="uviD"> ${uviLog}</span>`)
+      
     //Color coding the block for UVI based on results
     if (uviLog < 3) {
-      uviData.addClass("is-size-3 has-background-success");
+      $(".uviD").addClass("has-background-success");
     } else if (uviLog >= 3 && uviLog < 6) {
-      uviData.addClass("is-size-3 has-background-warning");
+      $(".uviD").attr("class","has-background-warning");
     } else if (uviLog >= 6 && uviLog < 8) {
-      uviData.addClass("is-size-3 tempHigh");
+      $(".uviD").attr("class","tempHigh");
     } else if (uviLog >= 8 && uviLog < 11) {
-      uviData.addClass("is-size-3 has-background-danger");
+      $(".uviD").attr("class","has-text-white has-background-danger");
     } else {
-      uviData.addClass("is-size-3 has-text-white tempSwelter");
+      $(".uviD").attr("class","has-text-white tempSwelter");
     }
-    newRowEl.append(name, temp, weather, humid, wind, uviData);
-     currentWeatherData.append(newRowEl);
+    
+    
 
     $("#fiveDayTable").empty();
     postForecast(data);
